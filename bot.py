94fb4770c680d9f88fcee86be7e3f3c34f762171
rafaelssantos2222-hub <rafaelssantos2222@gmail.com>
2026-05-02 22:13:15 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
-bot.py
+import requests
+import time
+import random
+
+# 🔑 coloque seu token aqui (BotFather)
+TOKEN = "8491561897:AAG2K1jTho6YIg3waHsqpi3VNMXkGI0Va6Q"
+
+# 📢 coloque seu canal aqui
+CANAL = "@promocoesrelampag1"
+
+promocoes = [
+    "🔌 Carregador turbo baratinho hoje! https://meli.la/1rJL3ee",
+    "🎧 Fone Bluetooth em oferta! https://meli.la/2NWAaXM",
+    "📱 Celular com desconto! https://meli.la/1Kv6msC"
+]
+
+while True:
+    msg = random.choice(promocoes)
+
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={"chat_id": CANAL, "text": msg}
+    )
+
+    time.sleep(60 * 30)
